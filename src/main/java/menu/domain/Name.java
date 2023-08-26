d834302dd943d1e774fd20cf793a243aefaab176
nuyh <hjaehyun25@gmail.com>
2023-08-26 15:35:46 +0900

    feat: 이름 도메인 구현 및 테스트

---
+++ src/main/java/menu/domain/Name.java
@@ -0,0 +1,22 @@
+package menu.domain;
+
+public class Name {
+
+    private static final int NAME_LOWER_BOUND = 2;
+    private static final int NAME_UPPER_BOUND = 4;
+
+    private final String value;
+
+    public Name(final String value) {
+        final String trimmed = value.trim();
+
+        validate(trimmed);
+        this.value = trimmed;
+    }
+
+    private void validate(final String value) {
+        if (value.length() < NAME_LOWER_BOUND || value.length() > NAME_UPPER_BOUND) {
+            throw new IllegalArgumentException("[ERROR] 이름은 2 ~ 4글자만 가능합니다.");
+        }
+    }
+}
---
+++ src/test/java/menu/domain/NameTest.java
@@ -0,0 +1,28 @@
+package menu.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class NameTest {
+
+    @DisplayName("앞 뒤 공백을 제외한 2 ~ 4글자로 생성할 수 있다")
+    @ValueSource(strings = {"이름", "이름이름", "    이름  ", "  이름이름  "})
+    @ParameterizedTest
+    void construct(final String name) {
+        assertDoesNotThrow(() -> new Name(name));
+    }
+
+    @DisplayName("앞 뒤 공백을 제외한 2 ~ 4글자가 아니면 예외가 발생한다")
+    @ValueSource(strings = {"이", "", "      ", "  이름이이름  "})
+    @ParameterizedTest
+    void construct_fail(final String name) {
+        assertThatThrownBy(() -> new Name(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름은 2 ~ 4글자만 가능합니다.");
+    }
+
+}
